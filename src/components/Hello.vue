bd8d5e119c21bdd5287e1ead2c1d635669a1d027
Wyatt Williams <Wyatt Williams>
2017-06-27 20:42:57 -0700

    Parallax working, images working, and functional GoodReads Api

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,38 +1,60 @@
 <template>
   <body>
-    <div class="divider"></div>
-     <div class="section">
-       <h5>Section 1</h5>
-       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in semper dui. Pellentesque dictum, dolor non volutpat cursus, nisi leo suscipit augue, eget egestas massa ipsum at ex. Mauris ultrices tincidunt lorem eu ullamcorper. Suspendisse eleifend ac sem vitae efficitur. Aliquam euismod lacus sit amet justo volutpat ultricies et ac enim. Integer euismod est eget placerat ultrices. Praesent convallis consectetur elit, id tempus nunc viverra eget.
-       Sed ultricies tristique fringilla. Phasellus malesuada venenatis lectus sit amet volutpat. Nulla rutrum fermentum ullamcorper. Praesent tempor sit amet erat in egestas. Vivamus sollicitudin tellus sed purus consectetur tempus. Cras ut nisi efficitur, sollicitudin nisl id, dictum felis. Nam vel urna quis nisl fringilla scelerisque in eget est. Nulla nec turpis ut sem sodales fringilla et vel risus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer dictum congue lorem nec tempor.
-       Cras vitae dolor quis turpis rutrum tempus. Mauris sollicitudin fringilla dignissim. Suspendisse at semper libero. Pellentesque tempus faucibus tincidunt. Curabitur vitae consectetur nisl. Nulla vel pretium lectus. Proin lacinia mi justo, molestie venenatis ante ullamcorper eu. Aliquam mollis, elit eu varius feugiat, eros orci aliquam eros, eget convallis erat mauris vitae sapien. Vivamus hendrerit est nibh, a faucibus erat convallis commodo. Nam elementum metus non massa laoreet condimentum. Vivamus ultrices porttitor magna, eget congue ante lacinia vel. Vestibulum accumsan suscipit pulvinar. Sed eu porta neque. Morbi eu vulputate sem.
-       Integer vel finibus dolor. Etiam purus dui, rutrum in eros vitae, vulputate gravida lorem. Suspendisse eu commodo sem, ut tincidunt odio. Integer molestie consequat commodo. Mauris quis lobortis nulla. In hac habitasse platea dictumst. Nunc ut consequat nulla. In imperdiet viverra nulla, ut fermentum velit tristique scelerisque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla luctus tortor vitae arcu vestibulum, vitae efficitur urna hendrerit.
-       Curabitur sit amet nisl malesuada, scelerisque purus in, lobortis elit. Aenean bibendum mi sed augue dapibus, vel tristique est consectetur. Vivamus non tellus in dui porta varius at vel neque. Nulla elit erat, dignissim in venenatis in, commodo ultricies nibh. Ut ullamcorper magna id eros rhoncus, non iaculis sapien viverra. Duis a ligula lectus. Curabitur id tellus a diam rutrum consequat. Ut lacinia nec diam et fringilla. Integer sit amet dolor non lorem suscipit bibendum quis quis libero. </p>
-     </div>
-     <div class="divider"></div>
-     <div class="section">
-       <h5>Section 2</h5>
-       <p>Stuff</p>
-     </div>
-     <div class="divider"></div>
-     <div class="section">
-       <h5>Section 3</h5>
-       <p>Stuff</p>
-     </div>
+    <div class="parallax-container">
+      <div class="parallax">
+          <img src="../assets/Orion.jpg"/>
+      </div>
+    </div>
+    <div class = "section black">
+      <div class ="row">
+        <div class="col s6">
+          <h1 class="center-align" style="color:white;">Wyatt Williams</h1>
+          <p class="center-align" style="color:white;">Hi, I'm Wyatt Williams. I studied Physics and Astronomy at the University of Nevada Reno.
+          I'm currently in the transition of becoming a developer. My past experiences have all been computer science based. From sifting</p>
+        </div>
+        <div class="col s6">
+          <h1 class="center-align" style="color:white;">Reading</h1>
+        </div>
+      </div>
+    </div>
+    <div class="parallax-container">
+      <div class="parallax">
+          <img src="../assets/Moon3.jpg"/>
+      </div>
+    </div>
+    <div class="section white">
+      <div class="row">
+        <div class="col s12">
+          <h1>Experience</h1>
+        </div>
+      </div>
+    </div>
   </body>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Blake Sucks Eggs'
+  var parseString = require('xml2js').parseString
+  export default {
+    name: 'Hello',
+    data () {
+      return {}
+    },
+    created: function () {
+      this.$http.get('https://crossorigin.me/https://www.goodreads.com/review/list?v=2&id=68964386&key=cG9KiyzfxG31nxkfLXSvBw').then(function (response) {
+        parseString(response.body, function (err, result) {
+          console.log(err)
+          console.dir(result)
+        })
+        this.profile = response.data
+      })
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.parallax img {
+  width: 100%;
+}
+
 </style>
